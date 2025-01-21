5e4857f9d1270db4090e3671b828618c4e1b3949
xransum <xransum@users.noreply.github.com>
2025-01-21 18:41:18 -0500

    Added package.

---
+++ src/rfc_lookup/__init__.py
@@ -0,0 +1,1 @@
+"""Init file for the rfc_lookup package."""
---
+++ src/rfc_lookup/constants.py
@@ -0,0 +1,5 @@
+"""Constants for the rfc_lookup package."""
+
+RFC_ARCHIVE_URL = "https://www.rfc-editor.org/in-notes/tar/RFC-all.tar.gz"
+RFC_INDEX_URL = "https://www.ietf.org/download/rfc-index.txt"
+RFC_LATEST_URL = "https://www.ietf.org/rfc/rfc-index-latest.txt"
